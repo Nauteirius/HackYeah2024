eff6380ce5c9d2e6379e7c8113fc5858e066e4c5
alexd3rsan <alex.zeising@gmail.com>
2024-09-28 21:26:49 +0200

    Add split_data.split to upload_pipeline

--- app/main.py
+++ app/main.py
@@ -1,40 +1,21 @@
 import streamlit as st
-import time
-import json
-
-with open('sample_data.json', "r") as file:
-    data = json.load(file)
 
 def main():
+    st.file_uploader
     uploaded_file = st.file_uploader("Choose an MP4 file", type=["mp4"])
 
     if uploaded_file is not None:
         # metadata
-        # file_details = {
-        #     "Filename": uploaded_file.name,
-        #     "File type": uploaded_file.type,
-        #     "File size": f"{uploaded_file.size} bytes"
-        # }
+        file_details = {
+            "Filename": uploaded_file.name,
+            "File type": uploaded_file.type,
+            "File size": f"{uploaded_file.size} bytes",
+        }
 
-        # st.json(file_details)
+        st.json(file_details)
 
-        if st.button("Process"):
-            steps = [
-                "Przetwarzanie wideo",
-                "Przetwarzanie audio",
-                "Analizowanie wypowiedzi"
-            ]
-            
-            placeholders = [st.empty() for _ in steps]
-            
-            for i, step in enumerate(steps):
-                placeholders[i].text(step)
-            
-            for i, step in enumerate(steps):
-                time.sleep(2)
-                placeholders[i].text(f"{step} ✅")
+        print("Uploaded-file:", uploaded_file)
 
-    print(uploaded_file)
 
 if __name__ == "__main__":
     main()
---
+++ app/upload_pipeline/split_data.py
@@ -0,0 +1,51 @@
+import io
+from typing import NamedTuple
+from tempfile import NamedTemporaryFile
+
+import ffmpeg
+import numpy as np
+
+
+class AudioVideo(NamedTuple):
+    fps: int
+    width: int
+    height: int
+    frames: list[np.ndarray]
+    audio: io.BytesIO
+
+
+def split(video_bytes: io.BytesIO) -> AudioVideo:
+
+    with NamedTemporaryFile(mode="w+b") as tmp_input:
+        tmp_input.write(video_bytes.read())
+
+        file_name = tmp_input.file.name
+
+        probe = ffmpeg.probe(file_name)
+        probe_stream = next(s for s in probe["streams"] if s["codec_type"] == "video")
+        # TODO: somehow propagate errors if needed
+        fps = int(probe_stream["avg_frame_rate"].split("/")[0])
+        width = int(probe_stream["width"])
+        height = int(probe_stream["height"])
+
+        out, _ = (
+            ffmpeg.input(file_name)
+            .video.output("pipe:", format="rawvideo", pix_fmt="bgr24")
+            .run(capture_stdout=True)
+        )
+        frames_4d = np.frombuffer(out, np.uint8).reshape([-1, height, width, 3])
+        out, _ = (
+            ffmpeg.input(file_name)
+            .audio.output("pipe:", format="mp3", acodec="libmp3lame")
+            .run(capture_stdout=True)
+        )
+
+        mp3_bytes = io.BytesIO()
+        mp3_bytes.write(out)
+        mp3_bytes.seek(0)
+
+        frames = [
+            arr.squeeze(0) for arr in np.split(frames_4d, frames_4d.shape[0], axis=0)
+        ]
+
+    return AudioVideo(fps, width, height, frames, mp3_bytes)
